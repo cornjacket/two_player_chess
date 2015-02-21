a2fe36c6496865eb3bb609ed93477807fed2800c
David Taylor <taylor.david.ray@gmail.com>
2015-02-20 23:59:02 -0800

    Adds Pawn#captures method

--- lib/two_player_chess/pawn.rb
+++ lib/two_player_chess/pawn.rb
@@ -3,7 +3,6 @@ module TwoPlayerChess
 
 
   class Pawn < Piece
-  	# how do these accessors work since they are stored in the parent
 
     attr_accessor :color, :location
     attr_reader :first_move
@@ -12,6 +11,38 @@ module TwoPlayerChess
       #@color = color
       #@location = location
       @first_move = true
+    end
+
+    def captures
+      valid_captures = []
+      if color == :white
+      	possible_captures = [ [col-1,row+1], [col+1,row+1] ]
+        #return [ [col-1,row+1], [col+1,row+1] ]
+      else
+      	possible_captures = [ [col-1,row-1], [col+1,row-1] ]
+      	#return [ [col-1,row-1], [col+1,row-1] ]
+      end
+      possible_captures.each do |tuple|
+        valid_captures << tuple if on_board(tuple)
+      end      
+      valid_captures
+    end
+
+    private
+
+    def on_board(tuple)
+      if (tuple[0] >= 0 && tuple[0] <= 7 && tuple[1] >= 0 && tuple[1] <= 7)
+      	return true
+      end
+      false
+    end
+
+    def col
+      location[0]
+    end
+
+    def row
+      location[1]
     end
 
   end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -18,13 +18,50 @@ module TwoPlayerChess
       it "can be initialize with a location of [0,0]" do
         piece = Pawn.new(:black, [0,0])
         expect(piece.location).to eq [0,0]
-      end      
+      end
+
+      it "returns a list of valid positions that white can capture" do
+        piece = Pawn.new(:white, [4,1])
+        expect(piece.captures).to eq [ [3,2], [5,2] ]
+      end 
 
     end # context "#initialize"
 
 
 
+    context "#captures" do
 
+      it "returns a list of valid positions that white can capture" do
+        piece = Pawn.new(:white, [4,1])
+        expect(piece.captures).to eq [ [3,2], [5,2] ]
+      end      
+
+      it "returns a list of valid positions that are on the game board" do
+        piece = Pawn.new(:white, [0,1])
+        expect(piece.captures).to eq [ [1,2] ]
+      end 
+
+      it "returns a list of valid positions that are on the game board" do
+        piece = Pawn.new(:white, [4,7])
+        expect(piece.captures).to eq []
+      end
+
+      it "returns a list of valid positions that black can capture" do
+        piece = Pawn.new(:black, [4,1])
+        expect(piece.captures).to eq [ [3,0], [5,0] ]
+      end 
+
+      it "returns a list of valid positions that are on the game board" do
+        piece = Pawn.new(:black, [7,6])
+        expect(piece.captures).to eq [ [6,5] ]
+      end 
+
+      it "returns a list of valid positions that are on the game board" do
+        piece = Pawn.new(:black, [4,0])
+        expect(piece.captures).to eq []
+      end
+
+    end # context "#captures"
       
 
   end
