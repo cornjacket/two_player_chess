e36c8e125f81df6b5743c53c47068c1a2752817c
David Taylor <taylor.david.ray@gmail.com>
2015-02-21 18:19:27 -0800

    Adds Rook#captures

--- lib/two_player_chess/rook.rb
+++ lib/two_player_chess/rook.rb
@@ -10,7 +10,7 @@ module TwoPlayerChess
       super(color, location)
     end
 
-=begin
+
     def captures
       moves
     end
@@ -18,36 +18,21 @@ module TwoPlayerChess
 
     def moves
       valid_moves = []     
-      # first find positive diagonal moves
-      if col < row
-        start_col = col-col
-        start_row = row-col
-      else
-        start_col = col-row
-        start_row = row-row
-      end
+      # first vertical moves
       8.times do |i|
-        new_move = [start_col+i, start_row+i]
-        break if !on_board(new_move)
+        new_move = [col, i]
+        #break if !on_board(new_move) # later can be used to stop movement
         valid_moves << new_move if new_move != location
       end
-      # next find negative diagonal moves
-      if col < row
-        neg_start_col = col - col
-        neg_start_row = row + col
-      else
-        diff = 7 - row
-        neg_start_col = col - diff
-        neg_start_row = row + diff
-      end
+      # next find horizontal moves
       8.times do |i|
-        new_move = [neg_start_col+i, neg_start_row-i]
-        break if !on_board(new_move)
+        new_move = [i, row]
+        #break if !on_board(new_move) # later can be used to stop movement
         valid_moves << new_move if new_move != location
       end
       valid_moves
     end
-=end
+
 
   end
 
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -22,27 +22,27 @@ module TwoPlayerChess
 
     end # context "#initialize"
 
-=begin
+
     context "#captures" do
 
       it "returns a list of valid positions that can be captured from (3,4)" do
-        piece = .new(:white, [3,4])
-        expect(piece.captures).to eq [ [0,1], [1,2], [2,3], [4,5], [5,6], [6,7], [0,7], [1,6], [2,5], [4,3], [5,2], [6,1], [7,0] ]
+        piece = Rook.new(:white, [3,4])
+        expect(piece.captures).to eq [ [3,0], [3,1], [3,2], [3,3], [3,5], [3,6], [3,7], [0,4], [1,4], [2,4], [4,4], [5,4], [6,4], [7,4] ]
       end      
 
       it "returns a list of valid positions that can be captured from (4,3)" do
-        piece = .new(:white, [4,3])
-        expect(piece.captures).to eq [ [1,0], [2,1], [3,2], [5,4], [6,5], [7,6], [0,7], [1,6], [2,5], [3,4], [5,2], [6,1], [7,0] ]
+        piece = Rook.new(:white, [4,3])
+        expect(piece.captures).to eq [ [4,0], [4,1], [4,2], [4,4], [4,5], [4,6], [4,7], [0,3], [1,3], [2,3], [3,3], [5,3], [6,3], [7,3] ]
       end 
 
       it "returns a list of valid positions that can be captured from (1,6)" do
-        piece = .new(:white, [1,6])
-        expect(piece.captures).to eq [ [0,5], [2,7], [0,7], [2,5], [3,4], [4,3], [5,2], [6,1], [7,0] ]
+        piece = Rook.new(:white, [1,6])
+        expect(piece.captures).to eq [ [1,0], [1,1], [1,2], [1,3], [1,4], [1,5], [1,7], [0,6], [2,6], [3,6], [4,6], [5,6], [6,6], [7,6] ]
       end 
 
     end # context "#captures"
       
-
+=begin
     context "#moves" do
 
       it "returns a list of valid positions that can be moved from (3,4)" do
