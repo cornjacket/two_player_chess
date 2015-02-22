c298307cc48d12a2839d600f40d654e92fb1b6d8
David Taylor <taylor.david.ray@gmail.com>
2015-02-21 17:31:09 -0800

    Adds Knight#moves

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -45,7 +45,20 @@ module TwoPlayerChess
 
     context "#moves" do
 
+      it "returns a list of valid positions that can be moved to from (3,4)" do
+        piece = Knight.new(:white, [3,4])
+        expect(piece.moves).to eq [ [2,6], [1,5], [1,3], [2,2], [4,2], [5,3], [5,5], [4,6] ]
+      end                             #  -1,+2  -2,+1  -2,-1  -1,-2  +1,-2  +2,-1  +2,+1  +1,+2
 
+      it "returns a list of valid positions that can be moved to from (4,3)" do
+        piece = Knight.new(:white, [4,3])
+        expect(piece.moves).to eq [ [3,5], [2,4], [2,2], [3,1], [5,1], [6,2], [6,4], [5,5] ]
+      end 
+
+      it "returns a list of valid positions that can be moved to from (1,6)" do
+        piece = Knight.new(:white, [1,6])
+        expect(piece.moves).to eq [ [0,4], [2,4], [3,5], [3,7] ]
+      end 
 
 
     end # context "#moves"
