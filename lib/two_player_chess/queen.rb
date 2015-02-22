5b39ef72ce4a1c2bc80804dc9211424c65ba7cdf
David Taylor <taylor.david.ray@gmail.com>
2015-02-21 19:34:28 -0800

    Adds Queen#captures

---
+++ lib/two_player_chess/queen.rb
@@ -0,0 +1,67 @@
+#lib/two_player_chess/piece.rb
+module TwoPlayerChess
+
+
+  class Queen < Piece
+
+    attr_accessor :color, :location
+    
+    def initialize(color, location)
+      super(color, location)
+    end
+
+
+    def captures
+      moves
+    end
+
+    # Maybe this code should be refactored with Rook and Bishop and placed inside
+    # Piece so that it is common code instead copied in multiple places
+    def moves
+      valid_moves = []     
+      # first find positive diagonal moves
+      if col < row
+        start_col = col-col
+        start_row = row-col
+      else
+        start_col = col-row
+        start_row = row-row
+      end
+      8.times do |i|
+        new_move = [start_col+i, start_row+i]
+        break if !on_board(new_move)
+        valid_moves << new_move if new_move != location
+      end
+      # next find negative diagonal moves
+      if col < row
+        neg_start_col = col - col
+        neg_start_row = row + col
+      else
+        diff = 7 - row
+        neg_start_col = col - diff
+        neg_start_row = row + diff
+      end
+      # first vertical moves
+      8.times do |i|
+        new_move = [neg_start_col+i, neg_start_row-i]
+        break if !on_board(new_move)
+        valid_moves << new_move if new_move != location
+      end
+      8.times do |i|
+        new_move = [col, i]
+        #break if !on_board(new_move) # later can be used to stop movement
+        valid_moves << new_move if new_move != location
+      end
+      # next find horizontal moves
+      8.times do |i|
+        new_move = [i, row]
+        #break if !on_board(new_move) # later can be used to stop movement
+        valid_moves << new_move if new_move != location
+      end
+      valid_moves
+    end
+
+
+  end
+
+end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,71 @@
+# spec/piece_spec.rb
+require "spec_helper"
+
+module TwoPlayerChess
+  describe Queen do
+
+    context "#initialize" do
+      it "raises an error if initialized without 2 parameters" do
+        expect { Queen.new() }.to raise_error
+      end    
+
+      it "can be initialized with a color of :white" do
+        piece = Queen.new(:white, [0,0])
+        expect(piece.color).to eq :white
+      end
+
+      it "can be initialized with a location of [0,0]" do
+        piece = Queen.new(:black, [0,0])
+        expect(piece.location).to eq [0,0]
+      end
+
+
+    end # context "#initialize"
+
+
+    context "#captures" do
+
+      it "returns a list of valid positions that can be captured from (3,4)" do
+        piece = Queen.new(:white, [3,4])
+        expect(piece.captures).to eq [ [0,1], [1,2], [2,3], [4,5], [5,6], [6,7], [0,7], [1,6], [2,5], [4,3], [5,2], [6,1], [7,0], 
+                                       [3,0], [3,1], [3,2], [3,3], [3,5], [3,6], [3,7], [0,4], [1,4], [2,4], [4,4], [5,4], [6,4], [7,4] ]
+      end      
+
+      it "returns a list of valid positions that can be captured from (4,3)" do
+        piece = Queen.new(:white, [4,3])
+        expect(piece.captures).to eq [ [1,0], [2,1], [3,2], [5,4], [6,5], [7,6], [0,7], [1,6], [2,5], [3,4], [5,2], [6,1], [7,0],
+                                     [4,0], [4,1], [4,2], [4,4], [4,5], [4,6], [4,7], [0,3], [1,3], [2,3], [3,3], [5,3], [6,3], [7,3] ]
+      end 
+
+      it "returns a list of valid positions that can be captured from (1,6)" do
+        piece = Queen.new(:white, [1,6])
+        expect(piece.captures).to eq [ [0,5], [2,7], [0,7], [2,5], [3,4], [4,3], [5,2], [6,1], [7,0],
+                                       [1,0], [1,1], [1,2], [1,3], [1,4], [1,5], [1,7], [0,6], [2,6], [3,6], [4,6], [5,6], [6,6], [7,6] ]
+      end 
+
+    end # context "#captures"
+      
+=begin
+    context "#moves" do
+
+      it "returns a list of valid positions that can be moved from (3,4)" do
+        piece = Queen.new(:white, [3,4])
+        expect(piece.moves).to eq [ [0,1], [1,2], [2,3], [4,5], [5,6], [6,7], [0,7], [1,6], [2,5], [4,3], [5,2], [6,1], [7,0] ]
+      end      
+
+      it "returns a list of valid positions that can be moved from (4,3)" do
+        piece = Queen.new(:white, [4,3])
+        expect(piece.moves).to eq [ [1,0], [2,1], [3,2], [5,4], [6,5], [7,6], [0,7], [1,6], [2,5], [3,4], [5,2], [6,1], [7,0] ]
+      end
+
+      it "returns a list of valid positions that can be moved from (1,6)" do
+        piece = Queen.new(:white, [1,6])
+        expect(piece.moves).to eq [ [0,5], [2,7], [0,7], [2,5], [3,4], [4,3], [5,2], [6,1], [7,0] ]
+      end
+
+
+    end # context "#moves"
+=end
+
+  end
+end
