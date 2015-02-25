1469c61c58793b30c58d1ad28c0a516a66761719
David Taylor <taylor.david.ray@gmail.com>
2015-02-24 21:29:57 -0800

    Game#play waits for valid move before moving on to switching player

--- lib/two_player_chess/game.rb
+++ lib/two_player_chess/game.rb
@@ -31,21 +31,23 @@ module TwoPlayerChess
     def play
       puts "#{current_player.name} has randomly been selected as the first player"
       while true
-        board.formatted_grid
-        puts ""
-        puts solicit_move
-        from_x = get_move.to_i
-        puts solicit_move
-        from_y = get_move.to_i
-        puts solicit_move
-        to_x = get_move.to_i
-        puts solicit_move
-        to_y = get_move.to_i
-        puts "from (#{from_x},#{from_y}) => (#{to_x},#{to_y})"
+        begin
+          board.formatted_grid
+          puts ""
+          puts solicit_move
+          from_x = get_move.to_i
+          puts solicit_move
+          from_y = get_move.to_i
+          puts solicit_move
+          to_x = get_move.to_i
+          puts solicit_move
+          to_y = get_move.to_i
+          puts "from (#{from_x},#{from_y}) => (#{to_x},#{to_y})"
 
-        valid_move = board.valid_move?(current_player.color,from_x, from_y, to_x, to_y)
-        puts "valid_move = #{valid_move}"
-        board.move_piece(from_x, from_y, to_x, to_y) if valid_move != false
+          valid_move = board.valid_move?(current_player.color,from_x, from_y, to_x, to_y)
+          puts "valid_move = #{valid_move}"
+        end until valid_move != false
+        board.move_piece(from_x, from_y, to_x, to_y) #if valid_move != false
 
         if board.game_over
           puts game_over_message
